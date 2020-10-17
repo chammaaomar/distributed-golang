a25284c5a3e1aaa53eafc43d79bb84112f95a851
Omar Chammaa <omar@mistplay.com>
2020-10-17 01:03:45 -0400

    readAt and close tests

--- internal/log/store_test.go
+++ internal/log/store_test.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,16 +14,46 @@ var width = lengthWidth + uint64(len(write))
 func TestStoreAppendRead(t *testing.T) {
 	file, err := ioutil.TempFile("", "test_append_read")
 	require.NoError(t, err)
+	defer os.Remove(file.Name())
 
 	s, err := newStore(file)
 	require.NoError(t, err)
 
 	testAppend(t, s)
 	testRead(t, s)
+	testReadAt(t, s)
 
 	s, err = newStore(file)
 	require.NoError(t, err)
 	testRead(t, s)
+}
+
+// test the store file persists to disk upon closing
+func TestStoreClose(t *testing.T) {
+	file, err := ioutil.TempFile("", "test_close")
+	require.NoError(t, err)
+	defer os.Remove(file.Name())
+
+	s, err := newStore(file)
+	require.NoError(t, err)
+
+	_, _, err = s.Append(write)
+	require.NoError(t, err)
+
+	fileInfo, err := os.Stat(file.Name())
+	require.NoError(t, err)
+
+	beforeSize := fileInfo.Size()
+
+	err = s.Close()
+	require.NoError(t, err)
+
+	fileInfo, err = os.Stat(file.Name())
+	require.NoError(t, err)
+
+	afterSize := fileInfo.Size()
+
+	require.Equal(t, width, uint64(afterSize-beforeSize))
 }
 
 // tests appending several elements
@@ -43,5 +74,25 @@ func testRead(t *testing.T, s *store) {
 		require.NoError(t, err)
 		require.Equal(t, write, read)
 		offset += width
+	}
+}
+
+func testReadAt(t *testing.T, s *store) {
+	t.Helper()
+	var offset int64
+	for i := 1; i < 4; i++ {
+		p := make([]byte, width)
+		bytesRead, err := s.ReadAt(p, offset)
+		require.NoError(t, err)
+
+		require.Equal(t, width, uint64(bytesRead))
+
+		length := enc.Uint64(p[:lengthWidth])
+		require.Equal(t, uint64(len(write)), length)
+
+		read := p[lengthWidth:]
+		require.Equal(t, write, read)
+
+		offset += int64(width)
 	}
 }
